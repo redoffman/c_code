76e8769729a50543b45eda6f7efdc56488a323db
redoffman <redoffman@gmail.com>
2020-03-04 17:16:52 +0900

    complete!!!

--- hellow.c
+++ hellow.c
@@ -3,10 +3,9 @@
 int main()
 {
     printf( "Hellow World!!!\n");
-<<<<<<< HEAD
     printf( "Hellow branch!\n");
-    printf( "Hellow Branch  2 ")
+    printf( "Hellow Branch  2 ");
 
-=======
->>>>>>> 8e530ad07b8acf283d48d64d94aaac5b3ca8f7fd
+    printf( "Complete!! ")
+
 }
