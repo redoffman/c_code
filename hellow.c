328cbedbb6c962538b87fe1a24c49c74a2a2dfde
redoffman <redoffman@gmail.com>
2020-03-04 17:13:11 +0900

    no message

--- hellow.c
+++ hellow.c
@@ -3,7 +3,10 @@
 int main()
 {
     printf( "Hellow World!!!\n");
+<<<<<<< HEAD
     printf( "Hellow branch!\n");
     printf( "Hellow Branch  2 ")
 
+=======
+>>>>>>> 8e530ad07b8acf283d48d64d94aaac5b3ca8f7fd
 }
